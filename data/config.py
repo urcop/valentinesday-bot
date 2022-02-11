b190bd729766d3f1ff3781cb04304cfd6450b71f
urcop <kirillzama@list.ru>
2022-02-11 20:38:37 +0300

    added work with database

--- data/config.py
+++ data/config.py
@@ -7,4 +7,8 @@ env.read_env()
 BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
 ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
 IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
+DB_PASS = env.str('DB_PASSWORD')
+DB_USER = env.str('DB_USER')
+PORT = env.str('PORT')
+db = env.str('DATABASE')
 
--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,11 +1,21 @@
+import asyncpg.exceptions
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
 from keyboards.default import menu
-from loader import dp
+from loader import dp, db
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
+    try:
+        user = await db.add_user(
+            fullname=message.from_user.full_name,
+            username=message.from_user.username,
+            tg_id=message.from_user.id
+        )
+    except asyncpg.exceptions.UniqueViolationError:
+        user = db.select_user(tg_id=message.from_user.id)
+
     await message.answer(f"Привет, {message.from_user.full_name}!\n"
                          f"Какого типа отправить валентинку?", reply_markup=menu)
